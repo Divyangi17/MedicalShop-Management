b3575830781ae329e0c04af903cd50adbd4f2960
divyangi <divyangichaudhari17@gmail.com>
2024-03-13 12:26:34 +0530

    login functionality

--- MedicalShop Management/Form1.vb
+++ MedicalShop Management/Form1.vb
@@ -10,4 +10,12 @@
     Private Sub SellToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SellToolStripMenuItem.Click
         Sell.Show()
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        Login.Close()
+    End Sub
 End Class
---
+++ MedicalShop Management/Login.vb
@@ -0,0 +1,43 @@
+﻿Imports System.Data.OleDb
+
+Public Class Login
+    Dim connection As OleDbConnection
+    Dim ds As DataSet
+    Dim loginsuccess As Boolean
+    Private Sub Login_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim connection_string As New String("Data Source=localhost;" +
+                                             "password=dev4; User id=dev; " +
+                                             "Provider=ORAOLEDB.Oracle")
+        connection = New OleDbConnection(connection_string)
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            ds = New DataSet
+
+            Dim qry As New String("Select PWD from TBL_USER WHERE ID='" & TextBox1.Text & "'")
+            Dim adp As New OleDbDataAdapter(qry, connection)
+            adp.Fill(ds)
+
+            If TextBox2.Text = ds.Tables(0).Rows(0)("PWD") Then
+                loginsuccess = True
+                Form1.Show()
+                Me.Hide()
+            Else
+                loginsuccess = False
+            End If
+        Catch ex As Exception
+
+        End Try
+
+
+
+
+    End Sub
+
+
+
+    Private Sub Login_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        Form1.Show()
+    End Sub
+End Class
